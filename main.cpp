3d0e1e39ea007082904b19a9e01e2741a923cc29
KingTorchick14 <kingtorchick19@gmail.com>
2024-10-04 20:53:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    int x = 3;
+    int y = 4;
 
+    cout << x + y << endl;
     return 0;
 }
